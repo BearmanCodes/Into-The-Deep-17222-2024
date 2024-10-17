a8b45a269cffed911a811812c38f80ad040fe72a
Shawn20701 <shawnharrigan08@icloud.com>
2024-10-17 16:00:00 -0400

    added arm programming

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Op/MotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Op/MotorTest.java
@@ -9,13 +9,17 @@ import com.qualcomm.robotcore.hardware.Servo;
 @TeleOp(name="Motor Test")
 public class MotorTest extends LinearOpMode {
     float motorpower;
+    float motorpower2;
     @Override
     public void runOpMode() throws InterruptedException {
         DcMotorEx motor = hardwareMap.get(DcMotorEx.class, "motor");
+        DcMotorEx motor2 = hardwareMap.get(DcMotorEx.class, "motor");
         waitForStart();
         while (opModeIsActive()){
             motorpower = (gamepad1.right_trigger - gamepad1.left_trigger);
             motor.setPower(motorpower);
+            motorpower2 = (gamepad2.right_trigger - gamepad2.right_trigger);
+            motor2.setPower(motorpower2);
         }
     }
 }
