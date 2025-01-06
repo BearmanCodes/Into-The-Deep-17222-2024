65ef2a7b213f12e133e7a25737976aa45bc6a509
Bearman <idostuff1225@gmail.com>
2025-01-05 20:46:29 -0500

    Help Me
    Fixed some stuff. The ritalin is wearing off

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Op/MainDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Op/MainDrive.java
@@ -34,25 +34,26 @@ public class MainDrive extends LinearOpMode {
             }
             int armCurrPos = armCore.pvtArm.getCurrentPosition(); //Keeps var of arm pos for ref
             drivetrainCore.run(gamepad1);
-            if (armCurrPos >= servoActionTol
-            ) {
+            if (armCurrPos >= servoActionTol) {
                 servoCore.dpadRun(servoCore.currentGamepad2, servoCore.previousGamepad2);
             }
             switch (modeCore.MODE){ //Based on the mode set the arm to be in control or moving auto
                 case NORMAL_MODE:
                     armCore.trigger(gamepad2, armCurrPos); //Give arm control to driver
-                    modeCore.Mode_Handler(servoCore.currentGamepad, servoCore.previousGamepad); //Handle variables for reaching the top bar position (X)
+                    modeCore.modeHandler(servoCore.currentGamepad, servoCore.previousGamepad); //Handle variables for reaching the top bar position (X)
                     break; //Why I picked switch statements. Keeps you out of while loop hell
                 case MOVE_MODE:
-                    modeCore.Move_Init(armCore); //init for running to position
+                    modeCore.initMove(armCore); //init for running to position
                     int err = Math.abs(armCurrPos - modeCore.armTarget); //amount of ticks to go to target
                     boolean BREAKFREE = Math.abs((gamepad2.right_trigger - gamepad2.left_trigger)) >= freedomPower;
+                    dashTele.addData("FREEDOM POWER: ", freedomPower);
+                    dashTele.addData("BREAKFREE?: ", BREAKFREE);
+                    modeCore.teleMove(dashTele, err);
                     //This boolean decides whether or not to give control back to the driver
                     if (err <= errTolerance || BREAKFREE) {
                         armCore.pvtArm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
                         modeCore.MODE = ModeCore.RUNNING_MODE.NORMAL_MODE;
                     }
-                    modeCore.Move_Tele(dashTele, err);
                     break;
             }
         }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Op/ModeCore.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Op/ModeCore.java
@@ -16,7 +16,7 @@ public class ModeCore {
     public RUNNING_MODE MODE = RUNNING_MODE.NORMAL_MODE;
     public int armTarget, armVelocity;
 
-    public void Mode_Handler(Gamepad currGamepad1, Gamepad prevGamepad1){
+    public void modeHandler(Gamepad currGamepad1, Gamepad prevGamepad1){
         if (currGamepad1.x && !prevGamepad1.x) { //Demonstrative variables used, replace later please.
             //BAR HANDLER
             armTarget = 700;
@@ -35,13 +35,13 @@ public class ModeCore {
         }
     }
 
-    public void Move_Init(ArmCore armCore){
+    public void initMove(ArmCore armCore){
         armCore.pvtArm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         armCore.pvtArm.setTargetPosition(armTarget);
         armCore.pvtArm.setVelocity(armVelocity);
     }
 
-    public void Move_Tele(Telemetry dashTele, int err){
+    public void teleMove(Telemetry dashTele, int err){
         dashTele.addData("Arm Target: ", armTarget);
         dashTele.addData("Arm Velocity: ", armVelocity);
         dashTele.addData("Error: ", err);
