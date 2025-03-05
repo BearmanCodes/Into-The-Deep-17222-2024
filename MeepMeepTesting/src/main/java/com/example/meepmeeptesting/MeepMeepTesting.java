7d9ffb97bd4ac9a108cd8c938a2804e3c129fccb
Bearman <idostuff1225@gmail.com>
2025-03-04 23:00:00 -0500

    Help a bit

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -8,7 +8,11 @@ import org.rowlandhall.meepmeep.roadrunner.DefaultBotBuilder;
 import org.rowlandhall.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MeepMeepTesting {
-    public static Pose2d startPose = new Pose2d(-63.25, 72, Math.toRadians(270));
+
+    public static Pose2d startPose = new Pose2d(-16.5, 72, Math.toRadians(270));
+    //static Pose2d startPose =  new Pose2d(-63.25, 72, Math.toRadians(270));
+
+
 
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
@@ -17,32 +21,16 @@ public class MeepMeepTesting {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(56.855292234154874, 56.855292234154874, Math.toRadians(183.88477629466686), Math.toRadians(188.84453843478258), 17.24)
                 .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(startPose)
-                        .splineToConstantHeading(new Vector2d(-49, 55), Math.toRadians(90))
-                        .setTangent(0)
-                        .addDisplacementMarker(() -> {
-                            //open pincer and set wrist to grab
-                        })
-                        .lineToConstantHeading(new Vector2d(-49, 67.5))
-                        .addDisplacementMarker(() -> {
-
-                        })
-                        .setTangent(270)
                         .splineToConstantHeading(new Vector2d(-3, 65), Math.toRadians(0))
-                        .addDisplacementMarker(() -> {
-
-                            //wait for arm to finish in case it hasn't
-                        })
+                        //move the arm up here
                         .lineToConstantHeading(new Vector2d(-3, 59))
-                        .addDisplacementMarker(() -> {
-
-                        })
-                        .setTangent(180)
-                        .splineToConstantHeading(new Vector2d(-49, 55), Math.toRadians(90))
-                        .setTangent(0)
-                        .addDisplacementMarker(() -> {
-
-                        })
-                        .lineToConstantHeading(new Vector2d(-49, 67.5))
+                        //move the arm ALL the way up here, then let it out and move back
+                        .splineTo(new Vector2d(-42, 19.07), Math.toRadians(270))
+                        .splineToConstantHeading(new Vector2d(-45, 67.81), Math.toRadians(95.41))
+                        .splineToConstantHeading(new Vector2d(-51, 12.68), Math.toRadians(259.33))
+                        .splineToConstantHeading(new Vector2d(-54, 68.40), Math.toRadians(90.62))
+                        .back(5)
+                        .strafeRight(10)
                         .build());
 
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/goToSub.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/goToSub.java
@@ -19,6 +19,7 @@ import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 @Autonomous(name="Example Auto")
 public class goToSub extends LinearOpMode {
     Pose2d startPose =  new Pose2d(-63.25, 72, Math.toRadians(270));
+    public static Pose2d testPose = new Pose2d(-16.5, 72, Math.toRadians(270));
     public static int armVelocity = 4000;
     public static int armBack = 5;
     public static int armBar = 4550;
@@ -35,7 +36,8 @@ public class goToSub extends LinearOpMode {
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
         drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        drive.setPoseEstimate(startPose);
+        drive.setPoseEstimate(testPose);
+
         TrajectorySequence trajectory0 = drive.trajectorySequenceBuilder(startPose)
                 .splineToConstantHeading(new Vector2d(-49, 55), Math.toRadians(90))
                 .setTangent(0)
@@ -48,15 +50,14 @@ public class goToSub extends LinearOpMode {
                 .addDisplacementMarker(() -> {
                     servoCore.pincer.setPosition(servoCore.pincerClose);
                 })
-                .waitSeconds(0.1)
-                .addDisplacementMarker(() -> {
-                    sleep(1000);
+                .UNSTABLE_addTemporalMarkerOffset(1, () -> {
                     armCore.pvtMove(armVelocity, armBar, opModeIsActive(), dashTele);
                 })
                 .setTangent(270)
                 .splineToConstantHeading(new Vector2d(-3, 65), Math.toRadians(0))
                 .addDisplacementMarker(() -> {
                     while (ArmAutoCore.running){
+                        drive.update();
                         armCore.checkArm();
                     };
                     //wait for arm to finish in case it hasn't
@@ -65,10 +66,11 @@ public class goToSub extends LinearOpMode {
                 .addDisplacementMarker(() -> {
                     armCore.pvtArm.setPower(1);
                 })
-                .waitSeconds(1.2)
-                .addDisplacementMarker(() -> {
-                    servoCore.pincer.setPosition(servoCore.pincerOpen);
+                .UNSTABLE_addTemporalMarkerOffset(1, () -> {
                     armCore.pvtArm.setPower(0);
+                })
+                .UNSTABLE_addTemporalMarkerOffset(1, () -> {
+                    servoCore.pincer.setPosition(servoCore.pincerOpen);
                     armCore.pvtMove(armVelocity, armBack, opModeIsActive(), dashTele);
                 })
                 .setTangent(180)
@@ -76,6 +78,7 @@ public class goToSub extends LinearOpMode {
                 .setTangent(0)
                 .addDisplacementMarker(() -> {
                     while (ArmAutoCore.running){
+                        drive.update();
                         armCore.checkArm();
                     }
                     servoCore.pincer.setPosition(servoCore.pincerOpen);
@@ -84,6 +87,43 @@ public class goToSub extends LinearOpMode {
                 })
                 .lineToConstantHeading(new Vector2d(-49, 67.5))
                 .build();
+
+        TrajectorySequence firstTrajectory = drive.trajectorySequenceBuilder(testPose)
+                .addTemporalMarker(0.25, () -> {
+                    armCore.pvtMove(armVelocity, armBar, opModeIsActive(), dashTele);
+                })
+                .splineToConstantHeading(new Vector2d(-3, 65), Math.toRadians(0))
+                .addDisplacementMarker(() -> {
+                    while (ArmAutoCore.running){
+                        drive.update();
+                        armCore.checkArm();
+                    };
+                })
+                //move the arm up here
+                .lineToConstantHeading(new Vector2d(-3, 59))
+                .addDisplacementMarker(() -> {
+                    armCore.pvtArm.setPower(1);
+                })
+                .UNSTABLE_addTemporalMarkerOffset(1, () -> {
+                    armCore.pvtArm.setPower(0);
+                })
+                .UNSTABLE_addTemporalMarkerOffset(1, () -> {
+                    servoCore.pincer.setPosition(servoCore.pincerOpen);
+                    armCore.pvtMove(armVelocity, armBack, opModeIsActive(), dashTele);
+                })
+                //move the arm ALL the way up here, then let it out and move back
+                .splineTo(new Vector2d(-42, 19.07), Math.toRadians(270))
+                .splineToConstantHeading(new Vector2d(-45, 67.81), Math.toRadians(95.41))
+                .splineToConstantHeading(new Vector2d(-51, 12.68), Math.toRadians(259.33))
+                .splineToConstantHeading(new Vector2d(-54, 68.40), Math.toRadians(90.62))
+                .back(5)
+                .strafeRight(10)
+                .addDisplacementMarker(() -> {
+                    drive.setPoseEstimate(startPose);
+                    drive.followTrajectorySequenceAsync(trajectory0);
+                })
+                .build();
+
         waitForStart();
         drive.followTrajectorySequenceAsync(trajectory0);
         while (!isStopRequested()){
